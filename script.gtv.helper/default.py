123516ca80b83bbe23e10388f35e28300178e327
Zero0Q <elvissalihovic.es@gmail.com>
2025-05-08 18:36:37 +0800

    Update repository structure for proper directory browsing

--- script.gtv.helper/default.py
+++ script.gtv.helper/default.py
@@ -2,6 +2,7 @@ import sys
 import xbmc
 import xbmcaddon
 import xbmcgui
+import xbmcplugin
 from resources.lib.tmdb import TMDBHelper
 
 def main():
@@ -21,10 +22,31 @@ def main():
         if content_type and category:
             items = tmdb.get_items(content_type, category)
             if items:
-                # Create a window to display items
-                window = xbmcgui.Window(10000)
-                window.setProperty('GTV.TMDB.Items', items)
-                xbmc.executebuiltin('Container.Refresh')
+                # Create a directory window
+                handle = int(sys.argv[1])
+                xbmcplugin.setContent(handle, 'movies' if content_type == 'movies' else 'tvshows')
+                
+                # Add items to the directory
+                for item in items:
+                    list_item = xbmcgui.ListItem(label=item['title'])
+                    list_item.setInfo('video', {
+                        'title': item['title'],
+                        'year': item['year'],
+                        'plot': item['plot'],
+                        'rating': item['rating'],
+                        'mediatype': item['mediatype']
+                    })
+                    list_item.setArt({
+                        'poster': item['poster'],
+                        'fanart': item['fanart']
+                    })
+                    # Add playable flag and context menu
+                    list_item.setProperty('IsPlayable', 'true')
+                    url = f'plugin://plugin.video.themoviedb.helper?info=play&tmdb_id={item["tmdb_id"]}&type={content_type}'
+                    xbmcplugin.addDirectoryItem(handle=handle, url=url, listitem=list_item, isFolder=False)
+                
+                # End directory listing
+                xbmcplugin.endOfDirectory(handle)
 
 if __name__ == '__main__':
     main()
